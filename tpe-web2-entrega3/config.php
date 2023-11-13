eb5145bea09a9924856175acd29a5cc9b4b3ca5a
tito060804 <144065294+tito060804@users.noreply.github.com>
2023-11-13 16:04:11 -0300

    Delete tpe-web2-entrega3 directory

--- tpe-web2-entrega3/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    const MYSQL_USER = 'root';
-    const MYSQL_PASS = '';
-    const MYSQL_DB = 'db_bodega';
-    const MYSQL_HOST = 'localhost';
-    const JWT_KEY = '';
-    const JWT_EXP = 3600; // 1hs
